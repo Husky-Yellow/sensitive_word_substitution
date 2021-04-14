f718f495b84006e7d583d692ec8d316cb8fab79c
Husky-Yellow <473795376@qq.com>
2021-04-14 10:16:21 +0800

    feat(project): 基于Vue的DFAS算法实现敏感词替换

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # sensitive_word_substitution
+<p align="center">
+  <a href="https://github.com/vuejs/vue">
+    <img src="https://img.shields.io/badge/vue-2.6.10-brightgreen.svg" alt="vue">
+  </a>
+  <a href="https://github.com/ElemeFE/element">
+    <img src="https://img.shields.io/badge/element--ui-2.13.0-brightgreen.svg" alt="element-ui">
+  </a>
+
+</p>
+#### 介绍
+
 基于Vue的DFA算法实现敏感词替换
+
+**primary origin**<font size=4>**(js实现敏感词过滤算法):**</font>https://juejin.im/post/6844903641778749447
---
+++ views/test/test.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <el-input
+      placeholder="关键字"
+      v-model="likestr"
+      style="width:240px;"
+    ></el-input>
+    <el-button type="primary" icon="el-icon-plus" round @click="verStr"
+      >处理</el-button
+    >
+    <el-input
+      placeholder="关键字*"
+      v-model="prolikestr"
+      style="width:240px;"
+    ></el-input>
+    处理后：{{ verlikestr }}
+  </div>
+</template>
+
+<script>
+import { filterSensitiveWord, myReplace } from "@/utils/test";
+export default {
+  data() {
+    return {
+      likestr: "",
+      verlikestr: "",
+      prolikestr: "",
+    };
+  },
+  methods: {
+    verStr() {
+      const temp = filterSensitiveWord(this.likestr);
+      this.verlikestr = temp;
+      this.prolikestr = this.likestr;
+      temp.forEach((element) => {
+        let star_count = element.length;
+        let starstr = "";
+        for (let i = 0; i < star_count; i++) {
+          starstr += "*";
+        }
+        this.prolikestr = myReplace(this.prolikestr, element, starstr);
+      });
+    },
+  },
+};
+</script>
+
+<style></style>
